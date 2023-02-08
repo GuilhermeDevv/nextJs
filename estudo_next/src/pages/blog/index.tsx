f572d056773100ae3b5810942abaa4f9a1d61133
GuilhermeDevv <ddc3.0cf1@gmail.com>
2023-02-07 21:08:15 -0300

    imagem no nextjs

--- estudo_next/src/pages/blog/index.tsx
+++ estudo_next/src/pages/blog/index.tsx
@@ -1,9 +1,11 @@
 import React from "react";
+import Image from "next/image";
 
 export default function Blog() {
   return (
     <>
       <h1>Home do blog</h1>
+      <Image src="" alt="" />
     </>
   );
 }
