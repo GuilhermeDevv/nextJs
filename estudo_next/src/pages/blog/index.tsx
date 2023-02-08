f53774f2dc25e582d63a9718da96fca16c47c887
GuilhermeDevv <ddc3.0cf1@gmail.com>
2023-02-07 21:24:34 -0300

    Link do next

--- estudo_next/src/pages/blog/index.tsx
+++ estudo_next/src/pages/blog/index.tsx
@@ -1,11 +1,18 @@
 import React from "react";
 import Image from "next/image";
+import Link from "next/link";
 
 export default function Blog() {
   return (
     <>
       <h1>Home do blog</h1>
       <Image src="" alt="" />
+      <ul>
+        <li>
+          {/* teste */}
+          <Link href="/blog/sobre" />
+        </li>
+      </ul>
     </>
   );
 }
